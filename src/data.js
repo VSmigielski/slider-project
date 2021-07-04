21a1950982abba0b6f01d6ae431cf8d3e5c2110f
Veronica <veronicasmigielski@gmail.com>
2021-07-04 12:15:54 +0200

    Finished slider project

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const people = [
+    {
+      id: 1,
+      name: 'veronica smigielski',
+        title : 'full-stack, web developer',
+      image:
+        'https://media-exp1.licdn.com/dms/image/C5603AQEAuStN1IamPA/profile-displayphoto-shrink_400_400/0/1621939891181?e=1628121600&v=beta&t=BPYwf7j3jbMcpgH1U3_14tJyzkMvylHTZEH4P22eRqA',
+      quote:
+        'Motivates and helps others',
+    },
+    {
+      id: 2,
+      name: 'anthony-tien nhat Huynh ',
+        title : 'java, python developer',
+      image:
+        'https://media-exp1.licdn.com/dms/image/C5603AQE9UNsGQBwHpA/profile-displayphoto-shrink_400_400/0/1612337723178?e=1628121600&v=beta&t=8xzXN7u7nebzq_VSrR-RJI5RcDI_16-qNyHsQPLuzMI',
+      quote:
+        'Motivated, team-player, awesome at development',
+    },
+    {
+      id: 3,
+      name: 'Daniel Treichel',
+        title : 'java developer',
+      image:
+        'https://media-exp1.licdn.com/dms/image/C5603AQFNSXYDEOq7-g/profile-displayphoto-shrink_400_400/0/1598908130685?e=1628121600&v=beta&t=5rJsPCE7lv9G_lY2jXlkbF6TqwZH8encdF9jFD2_3-Y',
+      quote:
+        'Always willing to help others',
+    },
+    {
+      id: 4,
+      name: 'Ha Nguyen',
+        title : 'java developer',
+      image:
+        'https://media-exp1.licdn.com/dms/image/C4E03AQHmrpvckS15iw/profile-displayphoto-shrink_400_400/0/1587604769212?e=1628121600&v=beta&t=6UFsr_w3CyZ1uRUjeW9OKIJUxRwwG5UzYeuy9RE6x9s',
+      quote:
+        'Determined, self-taught, and amazing!',
+    },
+  ];
+  
+  export default people;
